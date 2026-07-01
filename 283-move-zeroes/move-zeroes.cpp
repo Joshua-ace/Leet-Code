c00dec0c4f6f2cc4c19e4548419b542b1edda502
Joshua-ace <ace.joshuabranch@gmail.com>
2026-07-01 21:08:39 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (19.08%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,17 +1,20 @@
 class Solution {
+
 public:
     void moveZeroes(vector<int>& nums) {
+        // while(left<nums.size() && nums[left]!=0){
+        //     left++;
+        // }
         int left = 0;
         while(left<nums.size() && nums[left]!=0){
             left++;
         }
         for(int right = left+1; right<nums.size(); right++){
-            if(nums[right]!=0){
-               swap(nums[left],nums[right]);
-               left++;
-            }
+        if(nums[right]!=0){
+            swap(nums[left],nums[right]);
+            left++;
         }
-        
+    }
         return ;
     }
 };
